205dd3f9b0afd28101eac1c2400d259e90dfa7b5
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2014-12-22 09:06:26 -0800

    Initial Commit

--- graphic/main.cpp
+++ graphic/main.cpp
@@ -1,91 +1,100 @@
 
-//
-// Disclamer:
-// ----------
-//
-// This code will work only if you selected window, graphics and audio.
-//
-// Note that the "Run Script" build phase will copy the required frameworks
-// or dylibs to your application bundle so you can execute it on any OS X
-// computer.
-//
-// Your resource files (images, sounds, fonts, ...) are also copied to your
-// application bundle. To get the path to these resource, use the helper
-// method resourcePath() from ResourcePath.hpp
-//
 
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
 // Here is a small helper for you ! Have a look.
 #include "ResourcePath.hpp"
-
-int main(int, char const**)
+class TileMap : public sf::Drawable, public sf::Transformable
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile(resourcePath() + "icon.png")) {
-        return EXIT_FAILURE;
+public:
+    
+    bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
+    {
+        // load the tileset texture
+        if (!m_tileset.loadFromFile(tileset))
+            return false;
+        
+        // resize the vertex array to fit the level size
+        m_vertices.setPrimitiveType(sf::Quads);
+        m_vertices.resize(width * height * 4);
+        
+        // populate the vertex array, with one quad per tile
+        for (unsigned int i = 0; i < width; ++i)
+            for (unsigned int j = 0; j < height; ++j)
+            {
+                // get the current tile number
+                int tileNumber = tiles[i + j * width];
+                
+                // find its position in the tileset texture
+                int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
+                int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
+                
+                // get a pointer to the current tile's quad
+                sf::Vertex* quad = &m_vertices[(i + j * width) * 4];
+                
+                // define its 4 corners
+                quad[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
+                quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+                quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
+                quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+                
+                // define its 4 texture coordinates
+                quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+                quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+                quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
+                quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+            }
+        
+        return true;
     }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile(resourcePath() + "cute_image.jpg")) {
-        return EXIT_FAILURE;
+    
+private:
+    
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+    {
+        // apply the transform
+        states.transform *= getTransform();
+        
+        // apply the tileset texture
+        states.texture = &m_tileset;
+        
+        // draw the vertex array
+        target.draw(m_vertices, states);
     }
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
-        return EXIT_FAILURE;
+    
+    sf::VertexArray m_vertices;
+    sf::Texture m_tileset;
+};
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800,600), "Tilemap");
+    const int level[] =
+    {
+        0, 1, 2, 7,
+        22, 13, 14, 7,
+        22, 13, 14, 7,
+        24, 25, 26, 7
+    };
+    TileMap map;
+    if(!map.load(resourcePath() + "tileset.png", sf::Vector2u(32,24), level, 4, 4))
+    {
+        return -1;
     }
-    sf::Text text("Hello SFML", font, 50);
-    text.setColor(sf::Color::Black);
-
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
-        return EXIT_FAILURE;
-    }
-
-    // Play the music
-    music.play();
-
-    // Start the game loop
     while (window.isOpen())
     {
-        // Process events
+        // handle events
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // Close window: exit
-            if (event.type == sf::Event::Closed) {
+            if(event.type == sf::Event::Closed)
                 window.close();
-            }
-
-            // Escape pressed: exit
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
-            }
         }
-
-        // Clear screen
+        
+        // draw the map
         window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
+        window.draw(map);
         window.display();
     }
-
-    return EXIT_SUCCESS;
+    return 0;
 }
